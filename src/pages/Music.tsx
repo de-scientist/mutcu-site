1027d64f5a2ce4088e4701a1a711b4d0475b2abd
de-scientist <gitaumark502@gmail.com>
2025-11-19 03:55:35 +0300

    import Navbar from "../components/Navbar";

--- src/pages/Music.tsx
+++ src/pages/Music.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./Navbar";
+import Navbar from "../components/Navbar";
 import PageHero from "../components/PageHero";
 import LeaderProfile from "../components/LeaderProfileM";
 import RolesSection from "../components/RolesM";
