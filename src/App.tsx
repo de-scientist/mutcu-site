02d1e754573cb1ecfff4b229a2f9ff1149b3a764
de-scientist <gitaumark502@gmail.com>
2025-11-19 04:02:36 +0300

    export the Layout function controller

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+// src/App.tsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Chairman from "./pages/Chairman";
+import Layout from "./components/Layout";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Ministries from "./pages/Ministries";
@@ -10,31 +10,30 @@ import Evangelism from "./pages/Evangelism";
 import Music from "./pages/Music";
 import Events from "./pages/Events";
 import Bible from "./pages/Bible";
+import Chairman from "./pages/Chairman";
 import Contact from "./pages/Contact";
 //import Committees from "./pages/Committees";
 
-function App() {
+const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <Navbar />
-
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/ministries" element={<Ministries />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/bible" element={<Bible />}></Route>
-        <Route path="/creative" element={<Creative />} />
-        <Route path="/evangelism" element={<Evangelism />} />
-        <Route path="/music" element={<Music />} />
-        <Route path="/chairman" element={<Chairman />} />
-        <Route path="/contact" element={<Contact />} />
-        {/* <Route path="/committees" element={<Committees />} /> */}
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="ministries" element={<Ministries />} />
+          <Route path="creative" element={<Creative />} />
+          <Route path="evangelism" element={<Evangelism />} />
+          <Route path="music" element={<Music />} />
+          <Route path="events" element={<Events />} />
+          <Route path="bible" element={<Bible />} />
+          <Route path="chairman" element={<Chairman />} />
+          <Route path="contact" element={<Contact />} />
+          {/* <Route path="committees" element={<Committees />} /> */}
+        </Route>
       </Routes>
-
-      <Footer />
     </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/components/Layout.tsx
@@ -0,0 +1,19 @@
+// src/components/Layout.tsx
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+const Layout: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
