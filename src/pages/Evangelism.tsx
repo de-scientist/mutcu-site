8a86ff3b0e55f9510822d2e1738049ed366cab36
de-scientist <gitaumark502@gmail.com>
2025-11-19 03:45:16 +0300

    import Navbar from "../components/Navbar";

--- src/pages/Evangelism.tsx
+++ src/pages/Evangelism.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./Navbar";
+import Navbar from "../components/Navbar";
 import PageHero from "../components/PageHero";
 import LeaderProfile from "../components/LeaderProfileE";
 import RolesSection from "../components/RolesE";
